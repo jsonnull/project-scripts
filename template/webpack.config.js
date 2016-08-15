410e09e23f33f7aeb92d8015b33c259e344f657e
Jason Nall <jsonnull@gmail.com>
2016-08-15 01:32:14 -0400

    Add webpack configuration and scripts.

---
+++ template/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  context: __dirname + '/src',
+  entry: './index',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
