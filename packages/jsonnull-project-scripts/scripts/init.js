b322f6b825457d5e3584079484c8709c22713251
Jason Nall <jsonnull@gmail.com>
2016-09-23 01:56:24 -0400

    Migrate packages to lerna monorepo.

--- packages/jsonnull-project-scripts/scripts/init.js
+++ packages/jsonnull-project-scripts/scripts/init.js
@@ -2,21 +2,27 @@ var path = require('path')
 var fs = require('fs-extra')
 var spawn = require('cross-spawn')
 
+// Scripts are called from bin/project-scripts and pass in the root dir and appName
 module.exports = function(root, appName) {
+  // Path to this dependency in node_modules
   var selfPath = path.join(root, 'node_modules', 'jsonnull-project-scripts')
 
+  // The apps package.json
   var appPackage = require(path.join(root, 'package.json'))
-  var templatePackage = require(path.join(selfPath, 'config', 'package.json'))
+  // The template package.json in config
+  var templatePackage = require(path.join(selfPath, 'package.template.json'))
 
+  // Merge the package.json files
   var packageJson = Object.assign({}, appPackage, templatePackage)
-
   fs.writeFileSync(
     path.join(root, 'package.json'),
     JSON.stringify(packageJson, null, 2)
   )
 
+  // Copy all files from the template into the root
   fs.copySync(path.join(selfPath, 'template'), root);
 
+  // Perform npm install on final project dir
   var proc = spawn('npm', ['install'], {stdio: 'inherit'})
   proc.on('close', function (code) {
     if (code !== 0) {
